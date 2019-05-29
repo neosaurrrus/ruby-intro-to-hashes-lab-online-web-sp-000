c33ce3b39c7de254403b27a23cc076f321b87c48
Lukie Kang <lukiek+f@gmail.com>
2019-05-29 22:00:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,7 @@ end
 def monopoly_with_third_tier
 	monopoly = {railroads: {
 			pieces: 4,
-			names: {reading_railroad: {}, pennsylvania_railroad: {}, b_and_o_railroad: {}
+			names: {reading_railroad: {}, pennsylvania_railroad: {}, b_and_o_railroad: {}, shortline: {}
 
 		},
 			rent_in_dollars: {
